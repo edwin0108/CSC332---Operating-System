d31969d17947a6c6fc2cb14fc16bbb3ef3885b5c
edwin0108 <qmpyv2000@hotmail.com>
2018-11-02 19:54:57 -0400

    small changes

--- Labs/Task4_Wai/rand_input.c
+++ Labs/Task4_Wai/rand_input.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
   int x = 10;
   int y = 4;
   FILE *inpf;
-  inpf = fopen("students.txt", "w");
+  inpf = fopen("input_file.txt", "w");
   for (int i=0; i<x; i++){
     for (int j=0; j<y; j++){
       fprintf(inpf,"%*d ",3, rand()%100);
