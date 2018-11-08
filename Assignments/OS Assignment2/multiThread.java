2bdf90905370f98b638769e65748ae387a2518d8
edwin0108 <qmpyv2000@hotmail.com>
2018-11-07 20:26:26 -0500

    add comments

--- Assignments/OS Assignment2/multiThread.java
+++ Assignments/OS Assignment2/multiThread.java
@@ -4,12 +4,16 @@ import java.util.concurrent.Semaphore;
 
 public class multiThread extends Thread {
 
+    //array for merging both file's content and perform sorting
     private List<Integer> array = new ArrayList<Integer>();
+
+    //limiting the # key to be 1
     static Semaphore lock = new Semaphore(1);
 
     File file1;
     File file2;
 
+    //multiThread class contain two file, where file1 is the mainfile and file2 is the file getting merge.
     public multiThread(File file1, File file2) {
         this.file1 = file1;
         this.file2 = file2;
@@ -25,20 +29,24 @@ public class multiThread extends Thread {
                 + lock.availablePermits());
         
             try {
+                //scan the mainfile to the array
                 Scanner scanner = new Scanner(file1);
                 while(scanner.hasNextInt()){
                     array.add(scanner.nextInt());
                 }
                 scanner.close();
 
+                //scan the second file to the array
                 Scanner scanner1 = new Scanner(file2);
                 while(scanner1.hasNextInt()){
                     array.add(scanner1.nextInt());
                 }
                 scanner1.close();
 
+                //sort the entire array
                 Collections.sort(array);
 
+                //write the array contents back to the mainfile
                 FileWriter write = null;
                 try {
                     write = new FileWriter("datafile.txt");
@@ -58,6 +66,8 @@ public class multiThread extends Thread {
             }
         
         finally {
+            //release the lock after sorting and file writing
+            //so the next thread can access
             System.out.println(file2 + " Releasing lock...");
             lock.release();
             System.out.println("Available Semaphore permits now: " 
@@ -76,8 +86,7 @@ public class multiThread extends Thread {
         File file2 = new File("new2.txt");
         File file3 = new File("new3.txt");
 
-        //Semaphore key = new Semaphore(1,true);
-
+        //calling threads
         multiThread thread1 = new multiThread(mainFile, file1);
         thread1.start();
         multiThread thread2 = new multiThread(mainFile, file2);
